a8cf5c501c59638ecc57010cc7b32708ddce8809
dohaa aymann <dohaslam3@gmail.com>
2024-12-02 23:36:02 +0200

    settings and edit profile added

--- lib/Presentation/Home_naivebar/Settings/ui/Settings_View.dart
+++ lib/Presentation/Home_naivebar/Settings/ui/Settings_View.dart
@@ -15,37 +15,41 @@ class SettingsView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 20),
+             SizedBox(height:30.h),
             Row(
               children: [
                 CircleAvatar(
-                  radius: 40,
+                  radius: 40.r,
                   backgroundImage: AssetImage('assets/dohaa.jpeg'), // Replace with your image path
                 ),
-                const SizedBox(width: 20),
+                 SizedBox(width: 20.w),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children:  [
                     Text(
                       'Doha Noor',
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 20.spMin,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 5),
+                    SizedBox(height: 5.h),
                     Text(
                       'dohanoor14@gmail.com',
                       style: TextStyle(
                         color: Colors.grey,
-                        fontSize: 14,
+                        fontSize: 14.spMin,
                       ),
                     ),
                   ],
                 ),
               ],
             ),
-            const SizedBox(height: 30),
+            SizedBox(height:8.h),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.h),
+              child: Divider(),
+            ),
             const SectionHeader(title: 'Account'),
             ProfileOption(
               icon: Icons.person,
@@ -63,6 +67,7 @@ class SettingsView extends StatelessWidget {
                 // Add your navigation logic
               },
             ),
+            SizedBox(height: 8.h),
             const SectionHeader(title: 'Settings'),
             ProfileOption(
               icon: Icons.language,
@@ -74,12 +79,23 @@ class SettingsView extends StatelessWidget {
             ),
             ProfileOption(
               icon: Icons.notifications,
-              trailingWidget: Switch(value: true, onChanged: (value){}),
+              trailingWidget: Switch(value: true, onChanged: (value){},),
               title: 'Notification',
               onTap: () {
                 // Add your navigation logic
               },
             ),
+            // SizedBox(height: 8.h),
+            // const SectionHeader(title: 'Help & Support'),
+            // ProfileOption(
+            //   icon: Icons.help_center_outlined,
+            //   trailingWidget: Icon(Icons.arrow_forward_ios, size:18.h, color: Colors.black),
+            //   title: 'About',
+            //   onTap: () {
+            //     Navigator.of(context).push(MaterialPageRoute(builder: (context) => LanguageView(),));
+            //   },
+            // ),
+            SizedBox(height: 8.h),
             const SectionHeader(title: 'Security'),
             ProfileOption(
               icon: Icons.delete,
@@ -88,19 +104,29 @@ class SettingsView extends StatelessWidget {
               onTap: () {
                 // Add your navigation logic
               },
+            ),ProfileOption(
+              icon: Icons.logout,
+              trailingWidget: Icon(Icons.arrow_forward_ios, size:18.h, color: Colors.black),
+              title: 'Logout',
+              onTap: () {
+                // Add your navigation logic
+              },
             ),
-            InkWell(
-              child: Container(
-                alignment: Alignment.center,
-                height: 50.h,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15.r)),
-                  color:ColorStyle.lightgray
-                ),
-                child: Text('Logout',style:TextStyle(fontSize: 22.spMin,fontWeight: FontWeight.bold,color: Colors.red),),
-              ),
-            )
+             // SizedBox(height:8.h),
+           Spacer(),
+            // InkWell(
+            //   child: Container(
+            //     alignment: Alignment.center,
+            //     height: 50.h,
+            //     width: double.infinity,
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.all(Radius.circular(15.r)),
+            //       color:ColorStyle.lightgray
+            //     ),
+            //     child: Text('Logout',style:TextStyle(fontSize: 22.spMin,fontWeight: FontWeight.bold,color: Colors.red),),
+            //   ),
+            // ),
+            // SizedBox(height: 20.h),
           ],
         ),
       ),
@@ -140,10 +166,17 @@ class ProfileOption extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // padding: EdgeInsets.symmetric(vertical: 3.h),
       child: ListTile(
         contentPadding: EdgeInsets.zero,
-        leading: Icon(icon, color: title == 'Delete account' ? Colors.red : ColorStyle.pink,size: 25.h,),
-        title: Text(title, style: TextStyle(fontSize:18.spMin)),
+        leading: Container(
+            padding: EdgeInsets.all(8.h),
+            decoration:BoxDecoration(
+              color: ColorStyle.lightgray,
+              borderRadius: BorderRadius.all(Radius.circular(10.r))
+            ),
+            child: Icon(icon, color: title == 'Delete account' ? Colors.red : ColorStyle.darkgray,size:25.h,)),
+        title: Text(title, style: TextStyle(fontSize:18.spMin,fontWeight: FontWeight.w500)),
         trailing:  trailingWidget,
         onTap: onTap,
       ),
